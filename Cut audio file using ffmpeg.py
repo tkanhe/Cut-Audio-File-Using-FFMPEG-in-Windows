a95e2bc0f099bf5adfa275531f582957ad915d32
TK <tkanhe@gmail.com>
2021-12-03 05:12:24 +0530

    Add files via upload

---
+++ Cut audio file using ffmpeg.py
@@ -0,0 +1,22 @@
+import os
+import subprocess
+from datetime import datetime
+
+input_file = r'C:\Users\Administrator\Downloads\Breathe Carolina - Nights.wav'
+
+start = '01:00.000'  # HOURS:MM:SS.MILLISECONDS
+end = '02:00.000'
+
+name, ext = os.path.splitext(input_file)
+output_file = name + '_new' + ext
+
+delta = datetime.strptime(end, '%M:%S.%f') - datetime.strptime(start, '%M:%S.%f')
+delta_sec = delta.seconds + delta.microseconds / 1000000
+
+subprocess.call(['ffmpeg', '-i', input_file, '-ss', start, '-t', str(delta_sec), '-acodec', 'copy', output_file])
+
+# Following didn't work if there is/are space/s in path
+# subprocess.call(f'ffmpeg -i {input_file} -ss {start} -t {delta_sec} -acodec copy {output_file}')
+
+# command line
+# !ffmpeg -i $input_file -ss $start -t $delta_sec -acodec copy $output_file
